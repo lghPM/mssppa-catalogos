a51ef704f8a5478e4092b02bff3c9eea0d0e06bc
METRO\luis.gardunoh <T3mpor@l-241>
2023-08-11 08:58:05 -0600

    ESQUELETO INICIAL CON ACTUATOR EJEMPLO
    JAVA 20 SPRING 3.1.2

---
+++ src/main/java/mx/gob/imss/cit/sppa/catalogos/MssppaCatalogosApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.imss.cit.sppa.catalogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssppaCatalogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssppaCatalogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/imss/cit/sppa/catalogos/MssppaCatalogosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.imss.cit.sppa.catalogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssppaCatalogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
